d96f283b91ebba2e8d89cd00a294d48b2f7822b4
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-17 15:08:04 -0300

    add: case 2: pop array and refact: printOptions

--- source/arrays/main.cpp
+++ source/arrays/main.cpp
@@ -3,40 +3,44 @@
 
 #include "arrays.h"
 #include "../utils/print.h"
+#include "const.h"
 
 using namespace std;
 
 void printOptions();
 void menu(int array[], int &size);
 
-
-const int TAM = 100;
-
-int main(){
+int main()
+{
     int size = 12;
-    int array[TAM]{1,2,3,4,5,6,7,8,9,10,11,12};
-    
+    int array[TAM]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
     printArray(array, size);
-    while (true){
+    while (true)
+    {
         menu(array, size);
     }
-    
 }
 
-void menu(int array[], int &size){
+void menu(int array[], int &size)
+{
     int option;
     printOptions();
     cin >> option;
 
-    switch (option){
-    case 1: 
-            int value;
-            print("Agregar elemento, valor: ");
-            cin >> value;
-            push(array, value, size);
-            printArray(array, size);
+    switch (option)
+    {
+    case 1:
+        int value;
+        print("Agregar elemento, valor: ");
+        cin >> value;
+        pushStart(array, value, size);
+        printArray(array, size);
         break;
     case 2:
+        println("Popeando array... ");
+        pop(array, size);
+        printArray(array, size);
         break;
     default:
         println("exit...");
@@ -44,10 +48,10 @@ void menu(int array[], int &size){
     }
 }
 
-void printOptions(){
+void printOptions()
+{
     printlnAll(
         "\nOptions: ",
-        "1. push",
-        "2. printAll"
-        );
+        "1. pushStart",
+        "2. Pop array");
 }
