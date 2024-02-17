f96e93dcfc352e617e1fbecf499eb4cdf9a86bb4
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-17 12:56:56 -0300

    create: printAll and printlnAll

--- source/utils/print.h
+++ source/utils/print.h
@@ -5,12 +5,31 @@
 
 using namespace std;
 
-template<typename T>
-void print(const T& output){
-    cout << output;
+template <typename T>
+void print(const T &output)
+{
+    cout << output << " ";
 }
-template<typename T>
-void println(const T& output){
+template <typename T>
+void println(const T &output)
+{
     cout << output << endl;
 }
+void printAll(){
+    println("");
+}
+template <typename T, typename... Args>
+void printAll(const T& t, const Args&... args){
+    print(t);
+    printAll(args...);
+}
+void printlnAll(){
+    print("");
+}
+template <typename T, typename... Args>
+void printlnAll(const T& t, const Args&... args){
+    println(t);
+    printlnAll(args...);
+}
+
 #endif
