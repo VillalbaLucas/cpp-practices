8d8ef01b8aeb6f1cca2d53b53e853bbcda3b4e31
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-17 12:23:51 -0300

    create: menu and options functions

--- source/arrays/main.cpp
+++ source/arrays/main.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+void printOptions();
+void menu(int array[], int &size);
+
+
 const int TAM = 100;
 
 int main(){
@@ -13,6 +17,34 @@ int main(){
     int array[TAM]{1,2,3,4,5,6,7,8,9,10,11,12};
     
     printArray(array, size);
-    remove(array, 1, size);
-    printArray(array, size);
+    while (true){
+        menu(array, size);
+    }
+    
+}
+
+void menu(int array[], int &size){
+    int option;
+    printOptions();
+    cin >> option;
+
+    switch (option){
+    case 1: 
+            int value;
+            print("Agregar elemento, valor: ");
+            cin >> value;
+            push(array, value, size);
+            printArray(array, size);
+        break;
+    case: 
+         
+    default:
+        println("exit...");
+        break;
+    }
+}
+
+void printOptions(){
+    println("\nOptions: ");
+    println("1. push");
 }
