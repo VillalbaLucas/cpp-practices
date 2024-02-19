0e6c2df97111430599652b926e5affda60cdd6ce
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-19 11:55:39 -0300

    add: swap function

--- source/utils/aritmethic.h
+++ source/utils/aritmethic.h
@@ -1,5 +1,14 @@
-#ifndef namespace
-#define namespace
+#ifndef ARITMETHIC_H
+#define ARITMETHIC_H
 
+#include <iostream>
+
+void swap(int& a, int& b);
+
+void swap(int& a, int& b){
+     a += b;
+     b = a-b;
+     a -= b;
+}
 
 #endif
