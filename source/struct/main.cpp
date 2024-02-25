e39b2064d55ae1c24729f4d1c757ffe7ad18141c
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-25 12:57:07 -0300

    add: prints structs in main

---
+++ source/struct/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "struct.h"
+
+using namespace std;
+
+int main()
+{
+	Alumno Juan{"Juan", 18, 42123616};
+	printStruct(Juan);
+	iteratorPrintStruct(clase);
+	return 0;
+}
