00e16897cf65c691a759469d37313526e7e1ea25
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-25 13:21:17 -0300

    create: cantDigits

--- source/utils/aritmethic.h
+++ source/utils/aritmethic.h
@@ -4,6 +4,17 @@
 #include <iostream>
 
 void swap(int& a, int& b);
+int cantDigit(int num);
+
+int cantDigit(int num){
+     int digits{0};
+
+     while (num > 0){
+          num/=10;
+          digits++;            
+     }
+     return digits;
+}
 
 void swap(int& a, int& b){
      a += b;
